78915d092c75083f4a3898c775def4b38163490a
William Seiner <william.seiner@live.no>
2019-10-14 12:25:46 +0200

    Update oblig2.c

--- ProgKjor/oblig2.c
+++ ProgKjor/oblig2.c
@@ -8,46 +8,74 @@
  *  @author  William Seiner
  */
 
-#include <stdio.h>
+#include <stdio.h>      // scanf, printf
 #include <ctype.h>
+#include <string.h>
+
 
 const int MAXREGNEOPERASJONER = 50;
 
 int main () {           // Starter programmet
     
     int i, antallOperasjoner = 0;
-    float tallene [MAXREGNEOPERASJONER], akkumulator=0, tall;
+    float tallene [MAXREGNEOPERASJONER], akkumulator=0, tall=0.0;
     char regneOperasjoner[MAXREGNEOPERASJONER], regneOperasjon;
     
     
     
     printf("Enkel kalkulator:\n\n");
     
+    printf("= %f\n\n", akkumulator);
+    
     do {
-        printf("= %f\n\n", akkumulator);
-        
-        printf(": "); scanf(" %c %f", &regneOperasjon, &tall);
+        printf("\n\n: "); scanf (" %c", &regneOperasjon);
         
         regneOperasjon = tolower(regneOperasjon);
         
-        
-        
         switch (regneOperasjon)  {
-            case '+':   akkumulator+=tall;                      break;
-            case '-':   akkumulator-=tall;                      break;
-            case '*':   akkumulator*=tall;                      break;
-            case '/':   akkumulator/=tall;                      break;
-            case 't':   akkumulator=tall;                       break;
+            case '+':
+                scanf(" %f", &tall);
+                printf("\n%f + %f", akkumulator, tall);
+                akkumulator+=tall;
+                printf(" = %f", akkumulator);                     break;
+            case '-':
+                scanf(" %f", &tall);
+                printf("\n%f - %f", akkumulator, tall);
+                akkumulator-=tall;
+                printf(" = %f", akkumulator);                     break;
+            case '*':
+                scanf(" %f", &tall);
+                printf("\n%f * %f", akkumulator, tall);
+                akkumulator*=tall;
+                printf(" = %f", akkumulator);                     break;
+            case '/':
+                scanf(" %f", &tall);
+                if (tall==0)
+                    printf ("\nDele på null er tull!\n\n");
+                else
+                    printf("\n%f / %f", akkumulator, tall);
+                    akkumulator/=tall;
+                    printf(" %f", akkumulator);                  break;
+            case 't':
+                scanf(" %f", &tall);
+                akkumulator=tall;                              break;
             case 'c':   akkumulator=0;                          break;
-            default:    printf("\nFeil! Ukjent kommando gitt\n");    break;
+            case 's':   printf("\nEnkel kalkulator stopper\n");      break;
+            default:    printf("\n Feil! Ukjent kommando gitt\n");   break;
         }
+        
+        regneOperasjoner[antallOperasjoner]+=regneOperasjon;
+        
+        tallene[antallOperasjoner]+=tall;
+        
         antallOperasjoner++;
+        
     } while(regneOperasjon!='s');
     
-    printf("\nOperasjonen og tallene involvert:  \n");
+    printf("\nOperasjonen og tallene brukt:  \n");
     for (i = 0;  i < antallOperasjoner;  i++)
-        printf("%c og tallet: %f", regneOperasjoner[i], tallene[i]);
-        
+        printf("\n%c og tallet: %f", regneOperasjoner[i], tallene[i]);
+    printf("\n\n");
     
     return 0;           //Avslutter programmet
 }
