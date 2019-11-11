b726562d47642753864fb2478393e9fae02566c8
William Seiner <william.seiner@live.no>
2019-11-11 13:49:41 +0100

    nki

--- ProgKjor/oblig4.c
+++ ProgKjor/oblig4.c
@@ -10,27 +10,29 @@
 #include <stdio.h>          // scanf, printf
 #include <stdbool.h>        // bool
 #include <ctype.h>          // toupper, isdigit, isalpha
+#include "LesData.h"            //  Verktøykasse for lesing av diverse data.
 
-#define MAXPERS 6;
-#define MAXOPPG 20;
+#define MAXPERS 6
+#define MAXOPPG 20
 
 struct Oppgave {
     char* navn;
     int antallTotalt,
     antallNaa;
-    hvem[MAXPERS];
+    int hvem[MAXPERS];
 };
 
+void fjernOppgave();
 void nyOppgave();
-void oppgaveLesData(struct Oppgave* oppgave);
-void skrivOppgaver();
-void oppgaveSkrivData(const struct Oppgave* oppgave);
 void ledigeOppgaver();
 bool oppgaveLedigPlass(const struct Oppgave* oppgave);
-void personerTilknyttesOppgave();
-void oppgaveTilknyttPersoner(struct Oppgave* oppgave)
-void fjernOppgave();
+void oppgaveLesData(struct Oppgave* oppgave);
+void oppgaveSkrivData(const struct Oppgave* oppgave);
 void oppgaveSlettData(struct Oppgave* oppgave);
+void oppgaveTilknyttPersoner(struct Oppgave* oppgave);
+void personerTilknyttesOppgave();
+void skrivMeny();
+void skrivOppgaver();
 
 
 struct Oppgave* gOppgavene[MAXOPPG];
@@ -61,122 +63,31 @@ int main ()  {
     return 0;
 }
 
-/**
- *  Leser tekst inn i medsendt char-array/tekst/string.
- *
- *  @param   tall   - Int-array for tall
- *  @param   n      - Maks antall ganger for-løkken skal gå
- *  @param   min    - Minimum for tallverdi fra array
- *  @param   max    - Maksimum for tallverdi fra array
- *
- *  @return  Antall tall fra int-array mellom min og max
- */
-
-int antallIArray(int tall[], const int n, const int min, const int max) {
-    int antall=0;
-    for(int i=0; i<n; i++){
-        if ((tall[i] >= min) && (tall[i] <= max)) antall++; // Hvis tallet er mellom min og max så øker antall med 1
+void fjernOppgave();
+void nyOppgave() {
+    if (gSisteOppgave>MAXOPPG) {
+        printf("Fuck you");
     }
-    return antall;     // Returnerer antall tall som er mellom min og max
-}
-
-
-/**
- *  Leser tekst inn i medsendt char-array/tekst/string.
- *
- *  @param   tall  -  int-array som fylles med innleste tall
- *  @param   n     -  Maks antall ganger for-løkken skal gå
- *
- *  @see lesTall()
- */
-void fyllArray(int tall[], const int n) {
-    for(int i=0; i<n; i++) tall[i]=lesTall("Skriv inn heltall ", 0, 10000);  // Bruker lesTall og legger inn i array
-}
-
-/**
- *  Leser og returnerer ett (upcaset) tegn.
- *
- *  @return  Ett (upcaset) tegn.
- */
-char lesKommando()  {
-    char tegn;
-    printf("\nSkriv kommando:  ");
-    scanf(" %c", &tegn);   getchar();   // Leser inn kommando fra brukeren
-    return (toupper(tegn));            // Returnerer kommando gjort om til stor bokstav
-}
-
-/**
- *  Leser og returnerer et tall mellom to gitte grenser.
- *
- *  @param   tekst  - Ledetekst til brukeren nÂr ber om input/et tall
- *  @param   min    - Minimum for innlest og godtatt tallverdi
- *  @param   max    - Maksimum for innlest og godtatt tallverdi
- *
- *  @return  Godtatt verdi i intervallet 'min - 'max'
- */
-int lesTall(const char tekst[], const int min, const int max)  {
-    int tall;
-    // Leser inn tall mellom min og max:
-    do  {
-        printf("\t%s (%i-%i):  ", tekst, min, max);
-        scanf("%i", &tall);     getchar();
-    } while (tall < min  ||  tall > max);
-    return tall;    // Returnerer gyldig innlest tall
-}
-
-/**
- *  Leser tekst inn i medsendt char-array/tekst/string.
- *
- *  @param   ledetekst  - Ledetekst som utskrift til brukeren om hva lese inn
- *  @param   tekst      - char-array som fylles med innlest tekst
- */
-void lesTekst(const char ledetekst[], char tekst[])  {
-    // Nullstiller arrayen tekst[]:
-    for (int i=0; i<STRLEN; i++) {
-        tekst[i]='\0';
-    }
-    printf("\t%s:  ", ledetekst);
-    fgets(tekst, STRLEN, stdin);             //  Leser inn tekst fra brukeren
-}
-
-/**
- *  Sjekker om innlest postnr og poststed er gyldig.
- *
- *  @param      tekst  - Tekst som brukeren har skrevet inn.
- *
- *  @return     Om innlest tekst er gyldig postnr og poststed eller ikke
- */
-bool sjekkTekst(const char tekst[]) {
-    
-    int antallTall=0, antallBokstaver=0, i;
-    
-    //Sjekker om de fire første tallene i arrayen er tall:
-    for (i=0; i<4; i++) {
-        if (isdigit(tekst[i])) {
-            antallTall++;
-        }
-    }
-    
-    // Sjekker om tegnene etter tallene og et mellomrom er bokstaver, mellomrom, bindestrek og punktum:
-    for (i=antallTall+1; i<tekst['\0']; i++) {
-        if (isalpha(tekst[i]) || tekst[i]=='.' || tekst[i]=='-' || tekst[i]==' ') {
-            antallBokstaver++;
-        }
-    }
-    
-    // Returnerer true hvis antall tall er 3 eller 4 tall, og antall bokstaver er mer enn 0, og at det er et mellomrom etter tallene:
-    if (((antallTall==3) || (antallTall==4)) && (antallBokstaver>0) && (tekst[antallTall]==' ')) {
-        return true;
-    }
-    // Returnerer false hvis ikke betingelsene er oppfylt:
     else {
-        return false;
+        persPeker = (struct Oppgave*) malloc(sizeof(struct Oppgave));
+        
+        personLesData(persPeker);            //  Leser personen sine data.
+        
+        persPeker->neste = personListe;      //  HEKTER INN FORREST !!!!! :
+        
+        personListe = persPeker;
     }
+    
 }
+void ledigeOppgaver();
+bool oppgaveLedigPlass(const struct Oppgave* oppgave);
+void oppgaveLesData(struct Oppgave* oppgave);
+void oppgaveSkrivData(const struct Oppgave* oppgave);
+void oppgaveSlettData(struct Oppgave* oppgave);
+void oppgaveTilknyttPersoner(struct Oppgave* oppgave);
+void personerTilknyttesOppgave();
 
-/**
- *  Skriver ut en meny med kommandoer som kan brukes i programmet.
- */
+
 void skrivMeny() {
     printf("Kommandomeny:\n");
     printf("F for å fylle array\n");
@@ -185,3 +96,5 @@ void skrivMeny() {
     printf("S for å sjekke om postnummer og poststed er gyldig\n");
     printf("Q for å stoppe programlmet\n");
 }
+
+void skrivOppgaver();
